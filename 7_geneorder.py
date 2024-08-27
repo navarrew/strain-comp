520cecb144422dc9a86865e75d56258feed41855
William Wiley Navarre <42005831+navarrew@users.noreply.github.com>
2024-08-27 16:23:25 -0400

    Update 7_geneorder.py

--- 7_geneorder.py
+++ 7_geneorder.py
@@ -6,6 +6,9 @@ import pandas as pd
 from typing import Iterator, Tuple
 
 def get_locations(cell_item):
+#this function takes the cluster_table cells and puts the gene order 
+#up front in sortable format
+#with leading zeros so 1 becomes 000001. 
 	first_cell_fragment = cell_item.split("]|")[1].rstrip()
 	cell_fragments = first_cell_fragment.split(", ")
 	position_list = []
@@ -16,10 +19,13 @@ def get_locations(cell_item):
 		position_list.append("_".join(positional_info).replace("_", ":", 1))
 	position_list.sort()
 	position_string = str(", ".join(map(str, position_list)))
+	#we put it back in brackets because it will sort higher than * in the Excel table.
+	#if we don't use brackets then the asterisks all float to the top when sorting in ascending order.
 	position_string = "[" + position_string + "]"
 	return position_string
 	
 def tree2list(directory: str) -> Iterator[Tuple[str, str, str]]:
+#this function grabs the names of all the files in a directory recursively through all levels
 	import os
 	for i in os.scandir(directory):
 		if i.is_dir():
@@ -32,28 +38,45 @@ def tree2list(directory: str) -> Iterator[Tuple[str, str, str]]:
 
 if __name__ == '__main__':
 
+#look through the tables directory for cluster_table files that aren't already formatted.
 	directory_list = list(tree2list('tables'))
 	filelist = []
 	for filename in directory_list:
 		if 'tables/cluster_table' in filename:
 			if '.tab' in filename:
-				filelist.append(filename)
+				if 'geneordered.tab' not in filename:
+					filelist.append(filename)
 	filelist.sort()
-	print(filelist)
-	for i, x in enumerate(filelist):
-		print(str(i+1) + '. ' + x)
-	
-	input_filepath_index = int(input('Which cluster table to convert? '))
-	input_filepath = filelist[input_filepath_index - 1]
-	input_filename = input_filepath.split('/')[-1][0:-4]
 
+#if there's more than one choice, send it to the user to decide
+	if len(filelist) >= 2:
+		for i, x in enumerate(filelist):
+			print(str(i+1) + '. ' + x)
+		input_filepath_index = int(input('Which cluster table to convert (enter #)? '))
+		input_filepath = filelist[input_filepath_index - 1]
+		input_filename = input_filepath.split('/')[-1][0:-4]
+
+#if there's no choices then exit
+	elif len(filelist) == 0:
+		print('Cannot find a tab file to work on!')
+		exit()
+
+#if there's only one choice then just work on that.
+	elif len(filelist) == 1:
+		input_filepath = filelist[0]
+		input_filename = input_filepath.split('/')[-1][0:-4]
+
+#read the clustertable into a pandas object called input_table
 	input_table = pd.read_csv(input_filepath, sep='\t', index_col='CLUSTER')
 
+#the metadata for each cluster ends with the column 'strain count'.  Split the table here.
 	end_of_annotations_index = input_table.columns.get_loc('strain count')
 	number_of_columns = input_table.shape[1]+1 #had to add one because it doesn't count the index column I guess...or its indexed by 0.
+	#this will grab the left half of the table with the annotations/metadata
 	annotations_table = input_table.iloc[:,0:end_of_annotations_index+1]
+	#this will grab the right half of the cluster table with the strain information
 	strain_data_table = input_table.iloc[:,(end_of_annotations_index+1):]	
-
+	#temporarily writing out the strain data.
 	strain_data_table.to_csv('temp1.tab', sep='\t')
 
 	with open('temp1.tab', 'r') as f:
